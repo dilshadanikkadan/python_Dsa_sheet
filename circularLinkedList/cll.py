756cc37567687b0f8d3c45a7a4779e7d638e2a33
dilshadanikkadan <officialdilshad3312@gmail.com>
2024-12-24 19:45:17 +0530

    update:on cll

--- circularLinkedList/cll.py
+++ circularLinkedList/cll.py
@@ -8,7 +8,7 @@ class CircularLinkedList:
         self.head = None        
         self.tail = None
         
-    # for simplifying the print process    ):
+    # for simplifying the print process ):
     def __iter__(self):
         curr = self.head
         while curr:
@@ -43,7 +43,38 @@ class CircularLinkedList:
             curr.next = new_node
             new_node.next = next_node       
             
-    
+    def pop(self,position = -1 ):
+        if position == 0:
+           if self.head == self.tail:
+               self.head.next = None
+               self.head = None
+               self.tail = None
+           else:
+               self.head = self.head.next
+               self.tail.next = self.head
+        elif position == -1:
+              if self.head == self.tail: 
+                  self.head.next = None
+                  self.head = None
+                  self.tail = None     
+              else:
+                  curr = self.head
+                  while curr is not None:
+                      if curr.next == self.tail:
+                          break
+                      curr = curr.next 
+                  curr.next = self.head
+                  self.tail = curr
+        else:
+            curr = self.head
+            index = 0
+            while curr and index < position - 1:
+                curr = curr.next
+                index += 1
+            next_node = curr.next
+            curr.next = next_node.next    
+                
+                                   
 cll = CircularLinkedList()
 cll.boot_cll()
 cll.insert_el(10)               
@@ -51,6 +82,8 @@ cll.insert_el(20,1)
 cll.insert_el(30,2)               
 cll.insert_el(40,3) 
 
+print([node.value for node in cll])   
+cll.pop(2)           
 print([node.value for node in cll])              
 
 
